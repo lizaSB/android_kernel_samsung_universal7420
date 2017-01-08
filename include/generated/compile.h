6fd7edab7cbc43938ffc4dd6f1f2d264a0008bf3
greck99 <gax0ice@gmail.com>
2017-01-08 17:56:36 +0000

    Merge N9200-CPK2 Source

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#1 SMP PREEMPT Sun Jan 8 17:50:54 UTC 2017"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "NOBLE-GRECK"
+#define LINUX_COMPILER "gcc version 4.9 20150123 (prerelease) (GCC) "
