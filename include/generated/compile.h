f28f8508c19f88dcafcb6e7dedbbae776c6f6841
greck99 <gax0ice@gmail.com>
2017-01-08 20:00:00 +0000

    Block: Add BFQ and FIOPS Schedulers, Optimize Deadline

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 0 */
+/* This file is auto generated, version 1 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#0 SMP PREEMPT Sun Jan 8 18:42:57 UTC 2017"
+#define UTS_VERSION "#1 SMP PREEMPT Sun Jan 8 19:56:49 UTC 2017"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "NOBLE-GRECK"
 #define LINUX_COMPILER "gcc version 6.1.1 20160711 (Linaro GCC Snapshot 6.1-2016.07) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.10.61-gf4a90d5-dirty"
+#define UTS_RELEASE "3.10.61-g975ac64-dirty"
