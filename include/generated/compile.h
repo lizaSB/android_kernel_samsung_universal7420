709d6dfe74919607940534f8206702d246684f03
greck99 <gax0ice@gmail.com>
2017-01-08 19:12:45 +0000

    Change toolchain to linaro-64

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 1 */
+/* This file is auto generated, version 0 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#1 SMP PREEMPT Sun Jan 8 17:50:54 UTC 2017"
+#define UTS_VERSION "#0 SMP PREEMPT Sun Jan 8 18:42:57 UTC 2017"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "NOBLE-GRECK"
-#define LINUX_COMPILER "gcc version 4.9 20150123 (prerelease) (GCC) "
+#define LINUX_COMPILER "gcc version 6.1.1 20160711 (Linaro GCC Snapshot 6.1-2016.07) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.10.61-g4f4ad0e"
+#define UTS_RELEASE "3.10.61-gf4a90d5-dirty"
